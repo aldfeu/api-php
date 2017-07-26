2ffe870047bcae514c46444c113f440bd88ed258
Min Soo Kim <minsoo.kim@dacast.com>
2017-07-26 11:46:16 -0700

    Minor code cleanup

--- examples/vod/create.php
+++ examples/vod/create.php
@@ -2,7 +2,7 @@
 $api = new \Dacast\Api('YOUR_DACAST_API');
 $vod = $api->vod->uploadVod(
     [
-        "originalFilename" => 'NAME OF YOUR FILENAME',
+        "originalFilename" => 'NAME OF YOUR FILE',
         "file" => "./test/test.mp4" //vod source
     ],
     function ($progress) {
